42ab4d4b420eea930b0d95099e3f6e40f0db0d5e
selfpoised <selfpoised@gmail.com>
2021-02-04 20:32:57 +0800

    Update Smallest Subarray with a given sum (easy).java
    solution

--- 1. Pattern Sliding Window/Smallest Subarray with a given sum (easy).java
+++ 1. Pattern Sliding Window/Smallest Subarray with a given sum (easy).java
@@ -18,3 +18,40 @@ Problem Statement
     Output: 3
     Explanation: Smallest subarrays with a sum greater than or equal to '8' are [3, 4, 1] or [1, 1, 6].
 */
+
+public int solution(int[] array, int s){
+            int last_wnd_sum = 0;
+            int wnd_size_min = 0;
+            int wnd_start = 0;
+            int wnd_end = 0;
+
+            int i = 0;
+            while (i < array.length){
+                last_wnd_sum = last_wnd_sum + array[i];
+                if(last_wnd_sum >= s){
+                    wnd_end = i;
+
+                    int temp_wnd_size = wnd_end - wnd_start + 1;
+                    if(wnd_size_min== 0 || temp_wnd_size < wnd_size_min){
+                        wnd_size_min = temp_wnd_size;
+                        if(wnd_size_min == 1){
+                            return wnd_size_min;
+                        }
+                    }
+
+                    // minus window start element, then next element sum will be a new total window sum
+                    last_wnd_sum = last_wnd_sum - array[wnd_start];
+
+                    if(wnd_start == wnd_end){
+                        i++;
+                    } else{
+                        wnd_start++;
+                        last_wnd_sum = last_wnd_sum - array[wnd_end];
+                    }
+                } else{
+                    i++;
+                }
+            }
+
+            return wnd_size_min;
+        }
