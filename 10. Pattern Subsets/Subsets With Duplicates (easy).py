cb6845ed5eb69cf23bb657f448638a39c689ed58
selfpoised <selfpoised@gmail.com>
2021-04-13 18:43:53 +0800

    Create Subsets With Duplicates (easy).py

---
+++ 10. Pattern Subsets/Subsets With Duplicates (easy).py
@@ -0,0 +1,10 @@
+'''
+Problem Statement 
+Given a set of numbers that might contain duplicates, find all of its distinct subsets.
+Example 1:
+Input: [1, 3, 3]
+Output: [], [1], [3], [1,3], [3,3], [1,3,3]
+Example 2:
+Input: [1, 5, 3, 3]
+Output: [], [1], [5], [3], [1,5], [1,3], [5,3], [1,5,3], [3,3], [1,3,3], [3,3,5], [1,5,3,3] 
+'''
