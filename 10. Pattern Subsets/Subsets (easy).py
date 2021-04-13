db06b68b7d9b24049099fb1b0d1b5c8fb7dddd1f
selfpoised <selfpoised@gmail.com>
2021-04-13 16:24:13 +0800

    Update Subsets (easy).py

--- 10. Pattern Subsets/Subsets (easy).py
+++ 10. Pattern Subsets/Subsets (easy).py
@@ -8,3 +8,34 @@ Example 2:
 Input: [1, 5, 3]
 Output: [], [1], [5], [3], [1,5], [1,3], [5,3], [1,5,3]
 '''
+'''
+Problem Statement
+Given a set with distinct elements, find all of its distinct subsets.
+Example 1:
+Input: [1, 3]
+Output: [], [1], [3], [1,3]
+Example 2:
+Input: [1, 5, 3]
+Output: [], [1], [5], [3], [1,5], [1,3], [5,3], [1,5,3]
+'''
+
+def find_subsets(input):
+    subsets = []
+    subsets.append([])
+
+    for number in input:
+        n = len(subsets)
+        for i in range(n):
+            t = subsets[i].copy()
+            t.append(number)
+            subsets.append(t)
+
+    return subsets
+
+def main():
+
+  print("Here is the list of subsets: " + str(find_subsets([1, 3])))
+  print("Here is the list of subsets: " + str(find_subsets([1, 5, 3])))
+
+
+main()
