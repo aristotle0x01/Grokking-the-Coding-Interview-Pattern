1e6c586ae71bb3679b78279405f4d38435d519f4
selfpoised <selfpoised@gmail.com>
2021-04-18 11:45:00 +0800

    Create Find all Missing Numbers (easy).py

---
+++ 5. Pattern Cyclic Sort/Find all Missing Numbers (easy).py
@@ -0,0 +1,45 @@
+'''
+Problem Statement 
+We are given an unsorted array containing numbers taken from the range 1 to ‘n’. 
+The array can have duplicates, which means some numbers will be missing. Find all those missing numbers.
+Example 1:
+Input: [2, 3, 1, 8, 2, 3, 5, 1]
+Output: 4, 6, 7
+Explanation: The array should have all numbers from 1 to 8, due to duplicates 4, 6, and 7 are missing.
+Example 2:
+Input: [2, 4, 1, 2]
+Output: 3
+Example 3:
+Input: [2, 3, 2, 1]
+Output: 4
+'''
+
+def find_all_missing_number(array):
+    index = 0
+    n = len(array)
+
+    while index < n:
+        i = array[index] - 1
+        if array[index] != (index + 1):
+            if array[i] == array[index]:
+                index = index + 1
+                continue
+
+            array[i], array[index] = array[index], array[i]
+        else:
+            index = index + 1
+
+    r = []
+    for i in range(len(array)):
+        if array[i] != (i+1):
+            r.append(i+1)
+    return r
+
+
+def main():
+  print(find_all_missing_number([2, 3, 1, 8, 2, 3, 5, 1]))
+  print(find_all_missing_number([2, 4, 1, 2]))
+  print(find_all_missing_number([2, 3, 2, 1]))
+
+
+main()
