2879aa772a0350b2eece73ef1e8abc4a803fad1b
selfpoised <selfpoised@gmail.com>
2021-05-06 14:35:06 +0800

    Create Two Single Numbers (medium).py

---
+++ 12. Bitwise XOR/Two Single Numbers (medium).py
@@ -0,0 +1,15 @@
+'''
+Problem Statement
+
+In a non-empty array of numbers, every number appears exactly twice except two numbers that appear only once.
+Find the two numbers that appear only once.
+
+Example 1:
+Input: [1, 4, 2, 1, 3, 5, 6, 2, 3, 5]
+Output: [4, 6]
+
+Example 2:
+Input: [2, 1, 3, 2]
+Output: [1, 3]
+
+'''
